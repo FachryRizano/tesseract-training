293db90ce6495443f80642ef2a5a513b865fa4ab
Fachry Rizano <fachry.rizano@gmail.com>
2021-10-02 13:27:41 +0700

    update training.sh

--- training.sh
+++ training.sh
@@ -1,13 +1,13 @@
 # Constants
-FONT_NAME="Jokerman Regular"
-MAX_PAGES=10
+FONT_NAME="gara Black"
+MAX_PAGES=200
 NUM_ITERATIONS=400
 
 # Remove the previosly generated training data
 rm -rf train/*
 
 # Generate training data
-tesstrain.sh --fonts_dir fonts --fontlist "$FONT_NAME" --lang eng --linedata_only --langdata_dir ./tesseract/langdata_lstm --tessdata_dir ./tesseract/tessdata \
+tesstrain.sh --fonts_dir /content/tesseract-training/fonts --fontlist "$FONT_NAME" --lang eng --linedata_only --langdata_dir ./tesseract/langdata_lstm --tessdata_dir ./tesseract/tessdata \
  --maxpages $MAX_PAGES --output_dir train
 
 # Extract the best trainned model
@@ -15,9 +15,9 @@ combine_tessdata -e ./tesseract/tessdata/eng.traineddata eng.lstm
 
 # Fine tune the model
 rm -rf output/*
-OMP_THREAD_LIMIT=8 lstmtraining --continue_from eng.lstm --model_output output/jokerman --traineddata tesseract/tessdata/eng.traineddata \
+OMP_THREAD_LIMIT=8 lstmtraining --continue_from eng.lstm --model_output /content/tesseract-training/output/"$FONT_NAME" --traineddata tesseract/tessdata/eng.traineddata \
 --train_listfile ./train/eng.training_files.txt --max_iterations $NUM_ITERATIONS
 
 # Combine the checkpoints and create the final model
-lstmtraining --stop_training --continue_from output/jokerman_checkpoint --traineddata ./tesseract/tessdata/eng.traineddata \
-	--model_output output/jokerman.traineddata
+lstmtraining --stop_training --continue_from /content/tesseract-training/output/"$FONT_NAME"_checkpoint --traineddata ./tesseract/tessdata/eng.traineddata \
+	--model_output /content/tesseract-training/output/$FONT_NAME.traineddata
